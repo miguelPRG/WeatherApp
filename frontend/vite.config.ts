6e1173419f897c2cf8f4b3fa56c7f0eeb086fb4b
miguelgoncalves2024 <miguelgoncalves2024@hotmail.com>
2025-10-19 19:26:16 +0100

    Every thing is done

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,5 @@
 /// <reference types="vitest" />
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
@@ -18,11 +18,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    poolOptions: {
-      threads: {
-        isolate: true,
-      }
-    },
     setupFiles: "./tests/setupTests.ts",
   },
   //Building Optimization
